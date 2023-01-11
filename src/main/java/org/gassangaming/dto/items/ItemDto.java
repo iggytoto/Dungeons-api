ac3fdbe001f12bab199c46f6162094850830f10a
iggytoto <iggytoto@gmail.com>
2023-01-11 21:51:31 +0300

    dto item updated

--- src/main/java/org/gassangaming/dto/items/ItemDto.java
+++ src/main/java/org/gassangaming/dto/items/ItemDto.java
@@ -3,6 +3,7 @@ package org.gassangaming.dto.items;
 import lombok.*;
 import org.gassangaming.dto.DtoBase;
 import org.gassangaming.model.item.Item;
+import org.gassangaming.model.item.ItemRarity;
 import org.gassangaming.model.item.ItemType;
 
 @Builder
@@ -17,12 +18,16 @@ public class ItemDto extends DtoBase {
     private Long id;
     private long userId;
     private ItemType itemType;
+    private ItemRarity rarity;
+    private String name;
 
     public static ItemDto of(Item i) {
         return builder()
                 .id(i.getId())
                 .userId(i.getUserId())
                 .itemType(i.getItemType())
+                .name(i.getName())
+                .rarity(i.getRarity())
                 .build();
     }
 
